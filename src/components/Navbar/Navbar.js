b27f28fe7a44de89c784842bfe57bf9963efc2c0
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-24 11:42:01 +0700

    Navbar content and initial styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,16 @@
 import styles from "./Navbar.module.css";
 
+import Container from "../UI/Container";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <div className={styles.navContainer}>
+        <span className={styles.navItem}>Projects.</span>
+        <span className={styles.navItem}>Sam.</span>
+      </div>
+    </Container>
+  );
 };
 
 export default Navbar;
