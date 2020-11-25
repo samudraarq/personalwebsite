cf36dcb3fd1b6b9485327626ef4ed9cc7f835d17
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-25 15:19:38 +0700

    screenshots

---
+++ src/components/Projects/Talikasih/Screenshots/Screenshots.js
@@ -0,0 +1,43 @@
+import styles from "./Screenshots.module.css";
+import SecondaryBg from "../../../UI/SecondaryBg";
+
+import upicon from "../../../../assets/projects/feather_arrow-up.svg";
+
+import discoverpage from "../../../../assets/projects/talikasih/discoverpage.png";
+import campaigndetailspage from "../../../../assets/projects/talikasih/campaigndetailspage.png";
+import myprofilepage from "../../../../assets/projects/talikasih/myprofilepage.png";
+import adminpanelpage from "../../../../assets/projects/talikasih/adminpanelpage.png";
+
+const Screenshots = () => {
+  return (
+    <SecondaryBg>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Screenshots.</h2>
+
+        <div className={styles.screenshots}>
+          <div className={styles.screenshot}>
+            <img src={discoverpage} alt="discoverpage" />
+            <span>Discover</span>
+          </div>
+          <div className={styles.screenshot}>
+            <img src={campaigndetailspage} alt="campaigndetailspage" />
+            <span>Campaign Details</span>
+          </div>
+          <div className={styles.screenshot}>
+            <img src={myprofilepage} alt="myprofilepage" />
+            <span>My Profile</span>
+          </div>
+          <div className={styles.screenshot}>
+            <img src={adminpanelpage} alt="adminpanelpage" />
+            <span>Admin Panel</span>
+          </div>
+        </div>
+        <button className={styles.upBtn}>
+          <img src={upicon} alt="up" />
+        </button>
+      </div>
+    </SecondaryBg>
+  );
+};
+
+export default Screenshots;
--- src/pages/Project/Talikasih.js
+++ src/pages/Project/Talikasih.js
@@ -3,6 +3,7 @@ import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Projects/Talikasih/Header/Header";
 import CreatedUsing from "../../components/Projects/Talikasih/CreatedUsing/CreatedUsing";
 import Summary from "../../components/Projects/Talikasih/Summary/Summary";
+import Screenshots from "../../components/Projects/Talikasih/Screenshots/Screenshots";
 
 const Talikasih = () => {
   return (
@@ -11,6 +12,7 @@ const Talikasih = () => {
       <Header />
       <CreatedUsing />
       <Summary />
+      <Screenshots />
       <Footer />
     </>
   );
