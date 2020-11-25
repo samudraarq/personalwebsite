34fd7508f02399a669ca42613502e62eca835dbc
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-25 08:43:46 +0700

    add banner image

--- src/components/Home/Projects/Projects.js
+++ src/components/Home/Projects/Projects.js
@@ -1,11 +1,29 @@
 import styles from "./Projects.module.css";
 import Container from "../../UI/Container";
 
+import talikasih from "../../../assets/home/talikasih-banner.png";
+import iwatch from "../../../assets/home/iWatch-banner.png";
+import anydonor from "../../../assets/home/anydonnor-banner.png";
+
 const Projects = () => {
   return (
     <Container>
       <div className={styles.container}>
-        <h2>Projects.</h2>
+        <h2 className={styles.title}>Projects.</h2>
+        <div className={styles.projects}>
+          <div className={styles.project}>
+            <img src={talikasih} alt="talikasih banner" />
+            <h3>Talikasih</h3>
+          </div>
+          <div className={styles.project}>
+            <img src={iwatch} alt="iwatch banner" />
+            <h3>iWatch</h3>
+          </div>
+          <div className={styles.project}>
+            <img src={anydonor} alt="anydonor banner" />
+            <h3>AnyDonor</h3>
+          </div>
+        </div>
       </div>
     </Container>
   );
