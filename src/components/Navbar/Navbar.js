a50f1b5d3ae66e229e80cafd1ce8620ce21b03c1
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-25 10:05:50 +0700

    activate navbar functionality

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,27 @@
 import styles from "./Navbar.module.css";
 
 import Container from "../UI/Container";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <Container>
       <div className={styles.navContainer}>
-        <span className={styles.navItem}>Projects.</span>
-        <span className={styles.navItem}>Sam.</span>
+        <NavLink
+          exact
+          to="/"
+          style={{ textDecoration: "none" }}
+          activeClassName={styles.active}
+        >
+          <span className={styles.navItem}>Projects.</span>
+        </NavLink>
+        <NavLink
+          to="/about"
+          style={{ textDecoration: "none" }}
+          activeClassName={styles.active}
+        >
+          <span className={styles.navItem}>Sam.</span>
+        </NavLink>
       </div>
     </Container>
   );
